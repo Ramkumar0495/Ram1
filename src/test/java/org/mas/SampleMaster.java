018d472c9ea1e6bce920db855ba5843ddb58994a
Ramkumar <ramkumar0495@gmail.com>
2023-02-24 16:12:12 +0530

    login

---
+++ src/test/java/org/mas/SampleMaster.java
@@ -0,0 +1,20 @@
+package org.mas;
+
+public class SampleMaster {
+	private void test1() {
+		
+     System.out.println("a");
+	}
+	private void test2() {
+		
+	     System.out.println("b");
+		}
+	private void test3() {
+		
+	     System.out.println("c");
+		}
+
+
+	
+
+}
